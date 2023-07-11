f433b3169466e857fc574cab0154843908222fce
Rabindranath Ferreira <ferreiralox@gmail.com>
2023-07-11 23:17:51 +0200

    agregando clase 21 passport con passport-github login de 3ros

---
+++ class-21-authorization-authentication-strategy-jwt-p2/auth-strategy-github-p1/src/config/passport.config.js
@@ -0,0 +1,52 @@
+const passport = require("passport");
+const GithubStrategy = require("passport-github2");
+const userModel = require("../model/user.model");
+
+const GITHUB_CLIENT_ID = "sadas";
+const GITHUB_CLIENT_SECRET = "asdv4q123";
+
+const initializePassport = () => {
+  passport.use(
+    "github",
+    new GithubStrategy(
+      {
+        clientID: GITHUB_CLIENT_ID,
+        clientSecret: GITHUB_CLIENT_SECRET,
+        callbackURL: "http://localhost:5000/api/session/github/callback",
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        try {
+          console.log("PROFILE INFO ******", profile);
+          let user = await userModel.findOne({ email: profile._json?.email });
+          if (!user) {
+            let addNewUser = {
+              first_name: profile._json.name,
+              last_name: "",
+              email: profile._json?.email,
+              age: 0,
+              password: "",
+            };
+            let newUser = await userModel.create(addNewUser);
+            done(null, newUser);
+          } else {
+            // ya existia el usuario
+            done(null, user);
+          }
+        } catch (error) {
+          return done(error);
+        }
+      }
+    )
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    let user = await userModel.findById({ _id: id });
+    done(null, user);
+  });
+};
+
+module.exports = initializePassport;
